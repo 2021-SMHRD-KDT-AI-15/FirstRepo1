d18b462ececa6ea295ae502f8a2e37a5503a8fc7
explorersm11 <explorersm@naver.com>
2023-11-28 11:29:34 +0900

    윤수민 : 프로젝트 기본 파일 커밋

---
+++ BB/src/main/java/com/ic/db/SqlSessionManager.java
@@ -0,0 +1,30 @@
+package com.ic.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionManager {
+
+	public static SqlSessionFactory sqlSessionFactory;
+	
+	static {
+		String resource = "com/ic/db/Mybatis-config.xml";
+		InputStream inputStream;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	} 
+	
+	public static SqlSessionFactory getFactory() {
+		return sqlSessionFactory;
+	}
+
+}
