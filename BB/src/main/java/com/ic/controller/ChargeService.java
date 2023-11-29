4949a6b5c9a92ebf981ccfd68059d926adc82907
smhdr <smhdr@115.23.24.215>
2023-11-29 15:29:56 +0900

    임지후 - 커밋

--- BB/src/main/java/com/ic/controller/ChargeService.java
+++ BB/src/main/java/com/ic/controller/ChargeService.java
@@ -8,7 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.swing.text.DefaultEditorKit.InsertContentAction;
 
+import com.ic.model.ChargeDAO;
+import com.ic.model.ChargeDTO;
 import com.ic.model.MemberDTO;
 
 
@@ -19,11 +22,24 @@ public class ChargeService extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 
-		HttpSession session = request.getSession();
-		MemberDTO dto = new MemberDTO();
-		dto = (MemberDTO)session.getAttribute("clinetInfo");
+		request.setCharacterEncoding("UTF-8");
 		
+		int charge_money = Integer.parseInt(request.getParameter("charge_money"));
+		
+		HttpSession session = request.getSession();
+		
+		MemberDTO dto = new MemberDTO();
+		
+		dto = (MemberDTO)session.getAttribute("clientInfo");
 		int member_id = dto.getMember_id();
+		
+		ChargeDAO dao = new ChargeDAO();
+		
+		ChargeDTO dto2 = new ChargeDTO(member_id, charge_money);
+		
+		dao.charge(dto2);
+		
+		
 		
 	}
 
